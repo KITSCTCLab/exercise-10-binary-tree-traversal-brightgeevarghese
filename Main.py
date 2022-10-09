e62f10089a24575026b40fd12d2b00939df65e20
Bright Gee Varghese <brightlsom@gmail.com>
2022-10-09 15:07:44 +0530

    Initial

--- Main.py
+++ Main.py
@@ -12,18 +12,58 @@ def insert(root, new_value) -> BinaryTreeNode:
         Finally, return the root.
         """
     # Write your code here
+    # if binary search tree is empty, make a new node and declare it as root
+    if root is None:
+        root = BinaryTreeNode(new_value)
+        return root
+
+    # binary search tree is not empty, so we will insert it into the tree
+    # if newValue is less than value of data in root, add it to left subtree and proceed recursively
+    if new_value < root.data:
+        root.left_child = insert(root.left_child, new_value)
+    else:
+        # if newValue is greater than value of data in root, add it to right subtree and proceed recursively
+        root.right_child = insert(root.right_child, new_value)
+    return root
 
 
 def inorder(root) -> None:
     # Write your code here
+    # if root is None,return
+    if root is None:
+        return
+    # traverse left subtree
+    inorder(root.left_child)
+    # traverse current node
+    print(root.data, end=' ')
+    # traverse right subtree
+    inorder(root.right_child)
 
 
 def preorder(root) -> None:
     # Write your code here
+    # if root is None return
+    if root is None:
+        return
+    # traverse root
+    print(root.data, end=' ')
+    # traverse left subtree
+    preorder(root.left_child)
+    # traverse right subtree
+    preorder(root.right_child)    
 
 
 def postorder(root) -> None:
     # Write your code here
+    # if root is None return
+    if root is None:
+        return
+    # traverse left subtree
+    postorder(root.left_child)
+    # traverse right subtree
+    postorder(root.right_child)
+    # traverse root
+    print(root.data, end=' ')    
 
 
 # Do not change the following code
